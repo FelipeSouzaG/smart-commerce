16fce10091521b6a36d2ffd55b423d7859a2d3ff
FelipeSouzaG <154008572+FelipeSouzaG@users.noreply.github.com>
2026-01-04 14:32:46 -0300

    Update page.tsx

--- src/app/domain/checkout/page.tsx
+++ src/app/domain/checkout/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { notFound } from 'next/navigation';
-
+export const runtime = 'edge';
 export default function CheckoutPlaceholder() {
     return notFound();
+
 }
